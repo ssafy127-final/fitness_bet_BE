be2d8bc0468fdc3ece3b3405aa169733c6fbd201
Huk9uri <huk9uri@gmail.com>
2024-10-31 16:43:43 +0900

    초기 설정

---
+++ FitnessBet/src/main/java/com/fitnessbet/FitnessBetApplication.java
@@ -0,0 +1,13 @@
+package com.fitnessbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessBetApplication.class, args);
+	}
+
+}
