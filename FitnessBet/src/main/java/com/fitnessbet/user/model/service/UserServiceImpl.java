3eb5ad26936cae8719ce6349e287114803fba46d
Huk9uri <huk9uri@gmail.com>
2024-11-11 17:59:56 +0900

    feat : 도전자 반환 selectChallenger 메소드 UserServiceImpl 추가
     추가로 @Transactional 어노테이션 추가

--- FitnessBet/src/main/java/com/fitnessbet/user/model/service/UserServiceImpl.java
+++ FitnessBet/src/main/java/com/fitnessbet/user/model/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.fitnessbet.user.model.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fitnessbet.user.model.dao.UserDao;
 import com.fitnessbet.user.model.dto.User;
@@ -17,6 +18,7 @@ public class UserServiceImpl implements UserService{
 	}
 	
 	@Override
+	@Transactional
 	public boolean registUser(User user) {
 		int result = userDao.insertUser(user);
 		return result == 1;
@@ -24,6 +26,7 @@ public class UserServiceImpl implements UserService{
 	}
 
 	@Override
+	@Transactional
 	public User authenticate(String id, String pw) {
 		User user = userDao.findById(id);
 		if(user != null) {
@@ -35,27 +38,39 @@ public class UserServiceImpl implements UserService{
 	}
 
 	@Override
+	@Transactional
 	public boolean rejectUser(String id) {
 		int result = userDao.deleteUser(id);
 		return result == 1;
 	}
 
 	@Override
+	@Transactional
 	public List<User> getList(User user) { // 유저 객체를 모두 담아서, classNum과 campus를 꺼내씀
 		List<User> userList = userDao.selectAll(user);
 		return userList;
 	}
 
 	@Override
+	@Transactional
 	public boolean approve(String id) {
 		int result = userDao.updateAccepted(id);
 		return result == 1;
 	}
 
 	@Override
+	@Transactional
 	public int countUser(User user) {
 		return userDao.countAll(user);
 	}
+
+	@Override
+	@Transactional
+    public User selectChallenger(User user) {
+       user.setRandomNum((int) (Math.random() * userDao.countAll(user)));
+        User challenger = userDao.selectChallenger(user);
+        return challenger;
+    }
 
 
 }
