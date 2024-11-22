f59d53b3a43bf6e6052b5cbf0b88d17c10095492
sohyun <ksh96611@naver.com>
2024-11-22 16:26:49 +0900

    feat: pointStore 로직 구현

--- FitnessBet/src/main/java/com/fitnessbet/product/model/dao/ProductDao.java
+++ FitnessBet/src/main/java/com/fitnessbet/product/model/dao/ProductDao.java
@@ -1,5 +1,18 @@
 package com.fitnessbet.product.model.dao;
 
+import java.util.List;
+
+import com.fitnessbet.product.model.dto.Product;
+
 public interface ProductDao {
+
+	List<Product> selectAllProduct();
+
+	int insertProduct(Product product);
+
+	Product selectOneProductById(int id);
+
+	boolean updateProduct(Product origin);
+
 
 }
--- FitnessBet/src/main/java/com/fitnessbet/product/model/dto/Product.java
+++ FitnessBet/src/main/java/com/fitnessbet/product/model/dto/Product.java
@@ -1,5 +1,56 @@
 package com.fitnessbet.product.model.dto;
 
 public class Product {
+	
+	private int id;
+	private String name;
+	private int price;
+	private String img;
+	private int delYn;
+	private int point;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public int getDelYn() {
+		return delYn;
+	}
+	public void setDelYn(int delYn) {
+		this.delYn = delYn;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", img=" + img + ", delYn=" + delYn
+				+ ", point=" + point + "]";
+	}
+	
+	
 
 }
